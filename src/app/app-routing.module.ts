6eb14f07bc1df3c8371f0d40ecd590d46bb73f89
Nishtha <Nishtha.Tripathi@WisetechGlobal.com>
2024-10-29 13:59:50 +0530

    added routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmployeeComponent} from "./component/employee/employee.component";
+import {DepartmentComponent} from "./component/department/department.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'department', component: DepartmentComponent },
+  { path: '', redirectTo: '/employee', pathMatch: 'full' }  // Redirect to 'employee' as the default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
